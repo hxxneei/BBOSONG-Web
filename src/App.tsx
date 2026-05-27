1512cf37db14f9d767a487800d69432249b0679c
hxxneei <zxcvb5120@naver.com>
2026-05-27 20:17:59 +0900

    fix: UI 복구 및 카카오맵 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,3 @@
-// import GlobalStyle from "./styles/GlobalStyles";
-// import FabricScanner from "./pages/FabricScanner";
-// import ResultPage from "./pages/ResultPage";
-// import Signup from "./pages/Signup";
-// import Login from "./pages/Login";
-// import ChatPage from "./pages/ChatPage";
-// import BottomNav from "./common/BottomNav";
-
-// import React from "react";
-// import { useState } from "react";
-// import { Routes, Route, useLocation } from "react-router-dom";
-// import Firstpage from "./pages/Firstpage";
-// import SignupComplete from "./pages/SignupCompelet";
-// import MainHome from "./pages/MainHome";
-// import MyPage from "./pages/MyPage";
-// import MapView from "./pages/MapView";
-// import ClosetPage from "./pages/ClosetPage";
-
-// import TopPage from "./pages/CategoryPage";
-// import ClosetDetailPage from "./pages/ClosetDetailPage";
-// import OAuthCallbackPage from "./pages/OAuthCallbackPage";
-// import FavoriteStoresPage from "./pages/FavoriteStoresPage";
-// import MyClosetPage from "./pages/MyClosetPage";
-
-// const App: React.FC = () => {
-//   const [chatStep, setChatStep] = useState(1);
-//   const location = useLocation();
-//   const hideNavPaths = [
-//     "/",
-//     "/login",
-//     "/signup",
-//     "/signup-compelet",
-//     "/category-card",
-//     "/closetpage",
-//     "/result",
-//   ];
-//   const isBaseHidePath = hideNavPaths.includes(location.pathname.toLowerCase());
-//   const isChatSubStep =
-//     location.pathname.toLowerCase() === "/chatpage" && chatStep !== 1;
-
-//   const shouldHideNav = isBaseHidePath || isChatSubStep;
-
-//   // const shouldHideNav = hideNavPaths.includes(location.pathname.toLowerCase());
-
-//   return (
-//     <>
-//       <GlobalStyle />
-//       <Routes>
-//         <Route path="/" element={<Firstpage />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/signup" element={<Signup />} />
-//         <Route
-//           path="/chatpage"
-//           element={<ChatPage onStepChange={(step) => setChatStep(step)} />}
-//         />
-//         <Route path="/fabric-scanner" element={<FabricScanner />} />
-//         <Route path="/result" element={<ResultPage />} />
-//         <Route path="/signup-compelet" element={<SignupComplete />} />
-//         <Route path="/main-home" element={<MainHome />} />
-//         <Route path="/mypage" element={<MyPage />} />
-//         <Route path="/mapview" element={<MapView />} />
-//         <Route path="/closetpage" element={<ClosetPage />} />
-//         <Route path="/category-card" element={<TopPage />} />
-//         <Route path="/my-closet/:id" element={<ClosetDetailPage />} />
-//         <Route path="/oauth/success" element={<OAuthCallbackPage />} />
-//         <Route path="/favorite-stores" element={<FavoriteStoresPage />} />
-//         <Route path="/my-closet" element={<MyClosetPage />} />
-//       </Routes>
-//       {!shouldHideNav && <BottomNav />}
-//     </>
-//   );
-// };
-
-// export default App;
-// 📄 src/App.tsx (최종 수정본)
 import GlobalStyle from "./styles/GlobalStyles";
 import FabricScanner from "./pages/FabricScanner";
 import ResultPage from "./pages/ResultPage";
@@ -85,7 +10,7 @@ import React from "react";
 import { useState } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Firstpage from "./pages/Firstpage";
-import SignupCompelet from "./pages/SignupCompelet";
+import SignupComplete from "./pages/SignupCompelet";
 import MainHome from "./pages/MainHome";
 import MyPage from "./pages/MyPage";
 import MapView from "./pages/MapView";
@@ -99,8 +24,7 @@ import MyClosetPage from "./pages/MyClosetPage";
 
 const App: React.FC = () => {
   const [chatStep, setChatStep] = useState(1);
-  const location = useLocation(); // 👈 main.tsx의 BrowserRouter 덕분에 이제 완벽 작동합니다!
-
+  const location = useLocation();
   const hideNavPaths = [
     "/",
     "/login",
@@ -110,12 +34,13 @@ const App: React.FC = () => {
     "/closetpage",
     "/result",
   ];
-
   const isBaseHidePath = hideNavPaths.includes(location.pathname.toLowerCase());
   const isChatSubStep =
     location.pathname.toLowerCase() === "/chatpage" && chatStep !== 1;
 
   const shouldHideNav = isBaseHidePath || isChatSubStep;
+
+  // const shouldHideNav = hideNavPaths.includes(location.pathname.toLowerCase());
 
   return (
     <>
@@ -130,7 +55,7 @@ const App: React.FC = () => {
         />
         <Route path="/fabric-scanner" element={<FabricScanner />} />
         <Route path="/result" element={<ResultPage />} />
-        <Route path="/signup-compelet" element={<SignupCompelet />} />
+        <Route path="/signup-compelet" element={<SignupComplete />} />
         <Route path="/main-home" element={<MainHome />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/mapview" element={<MapView />} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,7 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-// import App from "./App";
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-// );
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
 
 const KAKAO_MAP_KEY = import.meta.env.VITE_KAKAO_MAP_API_KEY;
 
